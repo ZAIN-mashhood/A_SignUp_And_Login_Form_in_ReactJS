02dc0632d5130723d3f152f15c1e99f4a7d27fef
ZAIN-mashhood <zainmashhood9966@gmail.com>
2024-06-25 22:50:20 +0500

    deploy react to github

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/A_SignUp_And_Login_Form_in_ReactJS/",
+  plugins: [react()],
 })

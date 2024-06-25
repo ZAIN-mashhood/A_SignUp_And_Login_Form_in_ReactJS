97296b6edf1b0c64a3129c84a09102ee2d53c31d
ZAIN-mashhood <zainmashhood9966@gmail.com>
2024-06-25 22:08:35 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/A_SignUp_And_Login_Form_in_ReactJS/"
+  base: "/A_SignUp_And_Login_Form_in_ReactJS/",
 })
